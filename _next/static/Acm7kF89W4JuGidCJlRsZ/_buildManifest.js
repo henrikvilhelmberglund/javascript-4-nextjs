edd1b3cc6b52f18c987f26ccbb915adfebcc4077
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-10-09 13:57:01 +0200

    Updates

---
+++ _next/static/Acm7kF89W4JuGidCJlRsZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-c89c1952367445d8.js"],"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
